2944a237da74cbbe8a1f38b65b17dac0c684dbce
Jeremy Vonderfecht <CesiumLifeJacket@gmail.com>
2015-08-17 11:41:46 -0700

    printing data for debug purposes

--- R/make_glyph.R
+++ R/make_glyph.R
@@ -111,6 +111,7 @@ make_glyph <- function(fig, type, lname, lgroup, data, args, axis_type_range,
   args[long_ind] <- NULL
 
   ## remove NAs in data at this point?
+  print(data)
   data <- na.omit(data)
   
   ## spec needs to point to corresponding data
