9dc656841f476ea4f4a56ba3623b7469abecdc59
Jeremy Vonderfecht <CesiumLifeJacket@gmail.com>
2015-08-17 11:31:58 -0700

    Attempt to remove NAs from the data

--- R/make_glyph.R
+++ R/make_glyph.R
@@ -111,7 +111,8 @@ make_glyph <- function(fig, type, lname, lgroup, data, args, axis_type_range,
   args[long_ind] <- NULL
 
   ## remove NAs in data at this point?
-
+  data <- na.omit(data)
+  
   ## spec needs to point to corresponding data
   data_names <- names(data)
   for(nm in data_names)
